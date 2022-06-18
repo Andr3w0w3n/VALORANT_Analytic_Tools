e606dfc56e1fcbbf6f12e8b48153067e630eaa25
Andrew Owen <47571927+Andr3w0w3n@users.noreply.github.com>
2022-06-18 13:23:41 -0600

    Start Coding Main

--- Map-Control/Valorant_MapControl/Valorant_MapControl/Main.cpp
+++ Map-Control/Valorant_MapControl/Valorant_MapControl/Main.cpp
@@ -14,13 +14,24 @@ int main() {
 
 	VideoCapture cap(path);
 	Mat img;
+	Mat imgGray, imgCanny;
 
 	while (true) {
 
 		cap.read(img);
 
-		imshow("Image", img);
+		//Canny(img, imgCanny, 50, 150);
+		
+		//cropped image is (y_Range, x_Range)
+		Mat cropped_image = img(Range(70, 430), Range(150, 330));
+		cvtColor(cropped_image, imgGray, COLOR_BGR2GRAY);
+		Mat rgbchannel[3];
+
+		//imshow("Image", img);
+		//imshow("Image Canny", imgCanny);
+		imshow("Image Gray", imgGray);
 		waitKey(1);
 	}
-
+	destroyAllWindows();
+	return 0;
 }
