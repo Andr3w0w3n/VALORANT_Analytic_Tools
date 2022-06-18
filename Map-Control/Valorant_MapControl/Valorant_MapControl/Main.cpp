7d00dfe0d60051228afc044c948bec75f8185eab
Andrew Owen <47571927+Andr3w0w3n@users.noreply.github.com>
2022-06-18 16:20:33 -0600

    Attempt figuring out contrast and brightness changes

--- Map-Control/Valorant_MapControl/Valorant_MapControl/Main.cpp
+++ Map-Control/Valorant_MapControl/Valorant_MapControl/Main.cpp
@@ -14,7 +14,7 @@ int main() {
 
 	VideoCapture cap(path);
 	Mat img;
-	Mat imgGray, imgCanny;
+	Mat imgCont, imgBright;
 
 	while (true) {
 
@@ -23,13 +23,23 @@ int main() {
 		//Canny(img, imgCanny, 50, 150);
 		
 		//cropped image is (y_Range, x_Range)
-		Mat cropped_image = img(Range(70, 430), Range(150, 330));
-		cvtColor(cropped_image, imgGray, COLOR_BGR2GRAY);
-		Mat rgbchannel[3];
+		Mat cropped_image = img(Range(70, 430), Range(80, 430));
+		
+		
+		//messing with the colors a bit
+		//cvtColor(cropped_image, imgGray, COLOR_BGR2GRAY);
+		
+		//attempted splitting the image into RGB to modify but found I could just adjust the brightness and contrast and get a similar result
+		//Mat rgbchannel[3];
+
+		cropped_image.convertTo(imgCont, 1, -2, 0);//changing contrast
+		imgBright = imgCont - Scalar(80, 80, 80);//changing brightness
+
 
 		//imshow("Image", img);
 		//imshow("Image Canny", imgCanny);
-		imshow("Image Gray", imgGray);
+		imshow("Image Contrasted", imgCont);
+		imshow("Image Brighted & Constrasted", imgBright);
 		waitKey(1);
 	}
 	destroyAllWindows();
